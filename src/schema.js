bae2e69cd792ab88fa4b7f5fc03a25d6871221aa
Edward Fernandez <75320417+epfrndz@users.noreply.github.com>
2023-01-29 00:42:10 -0800

    Create data array

---
+++ src/schema.js
@@ -0,0 +1,17 @@
+const contacts = [
+  {
+    id: '1',
+    firstName: 'Paul',
+    lastName: 'Lam',
+  },
+  {
+    id: '2',
+    firstName: 'Elon',
+    lastName: 'Musk'
+  },
+  {
+    id: '3',
+    firstName: 'Steve',
+    lastName: 'Jobs'
+  }
+]
